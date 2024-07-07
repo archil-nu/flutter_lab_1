b3a31c2aa03272f2ca718b76dd4f61510bf01876
achiko-nu <a.lelashvili@northeastern.edu>
2024-07-06 21:43:00 -0400

    Removed extra code

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:english_words/english_words.dart';
 
 import 'package:lab_1/model/product.dart';
 import 'package:lab_1/screens/my_home_page.dart';
@@ -25,36 +24,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-  var history = <WordPair>[];
   Product? product;
-
-  GlobalKey? historyListKey;
-
-  void getNext() {
-    history.insert(0, current);
-    var animatedList = historyListKey?.currentState as AnimatedListState?;
-    animatedList?.insertItem(0);
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  var favorites = <WordPair>[];
-
-  void toggleFavorite([WordPair? pair]) {
-    pair = pair ?? current;
-    if (favorites.contains(pair)) {
-      favorites.remove(pair);
-    } else {
-      favorites.add(pair);
-    }
-    notifyListeners();
-  }
-
-  void removeFavorite(WordPair pair) {
-    favorites.remove(pair);
-    notifyListeners();
-  }
 
   void createProduct(
       String name, String description, int availability, int price) {
